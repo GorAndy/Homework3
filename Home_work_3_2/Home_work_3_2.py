588ab3c7d77d9229b50c17852335ade93c881766
Andrey Gavryushov <gor.andy@gmail.com>
2017-03-31 00:28:38 +0300

    Домашнее задание 3

--- Home_work_3_2/Home_work_3_2.py
+++ Home_work_3_2/Home_work_3_2.py
@@ -1,56 +1,84 @@
 # -*- coding='utf8' -*-
 from urllib.parse import urlencode, urlparse
 import requests
-import json
-
-
-def get_status(user_id=None):
-    """Получение статуса пользователя"""
-    if user_id:
-        params['user_id'] = user_id
-    response = requests.get('https://api.vk.com/method/status.get', params)
-    print(response.json(), params)
-
-
 
 AUTHORIZE_URL = 'https://oauth.vk.com/authorize'
 VERSION = '5.62'
 APP_ID = 5951940
 
-auth_data = {
-    'client_id': APP_ID,
-    'display': 'mobile',
-    'response_type': 'token',
-    'scope': 'friends, status, video',
-    'v': VERSION,
-}
-print("?".join((AUTHORIZE_URL, urlencode(auth_data))))
+# Код для получения token
+# auth_data = {
+#     'client_id': APP_ID,
+#     'display': 'mobile',
+#     'response_type': 'token',
+#     'scope': 'friends, status, video',
+#     'v': VERSION,
+# }
+#
+# print("?".join((AUTHORIZE_URL, urlencode(auth_data))))
 
-token_url = "https://oauth.vk.com/blank.html#access_token=ada75321210bccf76cc06b1019be2badd6a639caedae3006dcde85fe28fd0e8e94bd3648d1e4d73e7e271&expires_in=86400&user_id=14414804"
+
+def get_list_my_friends(user_id):
+    """Создаем словарь с данными друзей пользователя с указанным в аргументе id"""
+    params['fields'] = 'nickname'
+    params['user_id'] = user_id
+    response = requests.get('https://api.vk.com/method/friends.get', params)
+    friends_list = response.json()['response']['items']
+    return friends_list
+
+
+def print_list_friends_of_friend(friend_fn, friend_ln, list_friends_of_friend):
+    """Печатает last name и first name друзей выбранного друга"""
+    print()
+    print('Друзья моего друга {} {}:'.format(friend_fn, friend_ln))
+    for _ in list_friends_of_friend:
+        print('{} {}'.format(_['last_name'], _['first_name']))
+
+
+def get_mutual_friends(source_uid, target_uid):
+    """Поиск пересечений друзей двух пользователей"""
+    params['source_uid'] = source_uid
+    params['target_uid'] = target_uid
+    response = requests.get('https://api.vk.com/method/friends.getMutual', params)
+    print('Общие друзья с моими друзьями')
+    list_mutual_friends = response.json()['response']
+    return list_mutual_friends
+
+
+def search_friend_by_id(user_id):
+    """Поиск друга в списке по id"""
+    for friend in list_my_friends:
+        if int(user_id) == int(friend['id']):
+            first_name = friend['first_name']
+            last_name = friend['last_name']
+    return first_name, last_name
+
+
+token_url = "https://oauth.vk.com/blank.html#access_token=c67e848665a395737b1b9ac0d2e7e86983844a44cceb4355998b22f9cb820b075cf06f40e13f478ddcc01&expires_in=86400&user_id=14414804"
 
 o = urlparse(token_url)
 fragments = dict(i.split('=') for i in o.fragment.split('&'))
 access_token = fragments['access_token']
 
-params = { 'access_token': access_token,
+params = {
+    'access_token': access_token,
            'v': VERSION,
-}
+        }
+
+list_my_friends = get_list_my_friends('')
+list_id_of_my_friends = []
+for friend in list_my_friends:
+    friends_of_my_friend = get_list_my_friends(friend['id'])
+    # print_list_friends_of_friend(friend['first_name'], friend['last_name'], friends_of_my_friend)
+    list_id_of_my_friends.append(friend['id'])
+
+print(list_id_of_my_friends)
+# get_mutual_friends(list_id_of_my_friends)
+print(get_mutual_friends('', '2857935'))
+# print(search_friend_by_id())
 
 
 
-# get_status()
-params['fields'] = 'nickname'
-# params['user_id'] = '310351456'
-response = requests.get('https://api.vk.com/method/friends.get', params)
-print(response.json())
-friends_list = response.json()['response']['items']
-print('Мои друзья:')
-friends_id_list = []
-for friend in friends_list:
-    friend.pop('online')
-    friend.pop('nickname')
-    friends_id_list.append(friend['id'])
-    print('{} {}'.format(friend['last_name'], friend['first_name']))
-print(friends_id_list)
+
 
 # Поиск общих друзей friends.getMutual c параметрами target_uid или target_uids - список id через запятую
